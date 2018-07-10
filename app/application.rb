3762de93656270cee15a10059e67650453c90967
Scott Weinstein <sweinstein27@gmail.com>
2018-07-10 16:15:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
     else
       resp.write "Route not found"
       resp.status = 404
